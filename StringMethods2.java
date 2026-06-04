8072bf7ca37dfda2b706c815827f0d62835127d1
Sirangi Sandeep <sirangisandeep08@gmail.com>
2026-06-04 20:56:51 +0530

    Add files via upload

---
+++ StringMethods2.java
@@ -0,0 +1,11 @@
+class StringMethods2
+{
+    public static void main(String args[])
+    {
+        char[] letters={'a','p','p','l','e'};
+        
+        System.out.println(String.valueOf(letters));
+        
+    }
+
+}   

6f2715a9fffc602b7e03254807dabe815ed6a617
Sirangi Sandeep <sirangisandeep08@gmail.com>
2026-06-04 20:54:13 +0530

    Add files via upload

---
+++ StringMethods3.java
@@ -0,0 +1,13 @@
+class StringMethods3
+{
+    public static void main(String args[])
+    {
+        String s="water";
+        System.out.println(s.indexOf('e'));
+
+        
+        
+        
+    }
+
+}   

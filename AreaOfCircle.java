71ff6c9f08fac1559d154da19a176550b47ad629
Sirangi Sandeep <sirangisandeep08@gmail.com>
2026-06-05 20:42:33 +0530

    Add files via upload

---
+++ AreaOfCircle.java
@@ -0,0 +1,11 @@
+class AreaOfCircle
+{
+    public static double Area1(int r)
+    {
+        return (3.14*r*r);
+    }
+    public static void main(String args[])
+    {
+        System.out.println(Area1(3));
+    }
+}
